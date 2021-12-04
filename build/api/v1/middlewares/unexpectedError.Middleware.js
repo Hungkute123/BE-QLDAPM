4ad8aa5ae902c82548f9f143964a00493bc00af7
Nguyễn Đình Hùng <nguyendinhhung152@gmail.com>
2021-12-04 21:11:48 +0700

    merge

--- build/api/v1/middlewares/unexpectedError.Middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.unexpectedError = void 0;
-function unexpectedError(error, req, res, next) {
-    // winston.error(error.message, error);
-    res.status(500).send({ messages: [{ server: error.message }], code: 500 });
-}
-exports.unexpectedError = unexpectedError;
