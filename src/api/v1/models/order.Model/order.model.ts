c009aa952515d740b6ff89eb8455f824785586e3
Nguyễn Đình Hùng <nguyendinhhung152@gmail.com>
2021-12-25 21:20:02 +0700

    NDHung - api get order by user and seller

--- src/api/v1/models/order.Model/order.model.ts
+++ src/api/v1/models/order.Model/order.model.ts
@@ -1,5 +1,5 @@
 import internal from 'stream';
-import { TBL_ORDER, TBL_PRODUCTS } from '../../../../constants/tables';
+import { TBL_ORDER, TBL_PRODUCTS, TBL_USER } from '../../../../constants/tables';
 import { database } from '../../../../start/connectDB';
 
 class OrderModel {
@@ -14,7 +14,30 @@ class OrderModel {
 	async addNewOrder(Order: any) {
 		return database.add(Order, TBL_ORDER);
 	}
-	
+	//lay danh sach don hang nguoi dung da dat
+	async getOrderByIDUser(IDUser: number) {
+		const rows = await database.load(
+			`select * from ${TBL_ORDER} where IDUser = ${IDUser}`
+		);
+		if (rows.length === 0) return null;
+
+		return rows;
+	}
+	//lay cac don hang nguoi dung dat san pham của seller dang ban
+	async getOrderOfSeller(IDUser: number) {
+		const rows = await database.load(
+			`select *, od.Quantity as QuantityOrder,od.Status as StatusOrder from ${TBL_ORDER} od join ${TBL_PRODUCTS} pd on pd.IDProduct = od.IDProduct where pd.IDUser = ${IDUser}`
+		);
+		if (rows.length === 0) return null;
+
+		return rows;
+	}
+	updateStatus(status: string, IDOrder: number) {		
+		const entity = { Status: status };
+		const condition = { IDOrder: IDOrder  };
+
+		return database.patch(entity, condition, TBL_ORDER);
+	}
 }
 export const orderModel = new OrderModel();
 
--- src/api/v1/routes/routersApi/order.Router.ts
+++ src/api/v1/routes/routersApi/order.Router.ts
@@ -14,13 +14,15 @@ import { orderController } from '../../controllers/order.Controller';
 
 //--------------------------------------------GET------------------------------------------
 orderRouter.get('/all', orderController.getAllOrder);
+orderRouter.get('/order-by-iduser', orderController.getOrderByIDUser);
+orderRouter.get('/order-of-seller', orderController.getOrderOfSeller);
 
 //--------------------------------------------POST-----------------------------------------
 orderRouter.post('/add-new-order', orderController.addNewOrder);
 
 //--------------------------------------------PATCH------------------------------------------
 //orderRouter.patch('/patch-product', orderController.patchProduct);
-
+orderRouter.patch('/update-status',  orderController.updateStatus);
 //--------------------------------------------PUT------------------------------------------
 
 //--------------------------------------------DELETE----------------------------------------
